ea1040aacf27050b73edcd632db59c838f2d354f
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-05-08 06:39:55 -0700

    1. Repetition Token #0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,4 +2,4 @@
 #regular expression that will match the above cases
 #accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/t{2,5}/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
