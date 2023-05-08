266beac3098b551acaf4b3d74f2fff3016a3c994
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-05-08 06:48:38 -0700

    2. Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -2,4 +2,4 @@
 # the regular expression that will match the above cases
 #  accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/hbt{1}n/).join
+puts ARGV[0].scan(/hb?t{1}n/).join
