6d5860487a12d8c21dd136c2467bfd1a49985489
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-10-03 16:18:27 -0700

    new month

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Client configuration file (w/ Puppet)
 file { '~/ssh/config':
-  ensure => 'present',
+  ensure  => 'present',
   content => "# SSH client configuration\n
               52.205.84.91\n
               IdentityFile ~/.ssh/school\n
               PasswordAuthentication no\n",
-  owner => 'ubuntu',
-  group => 'ubuntu',
-  mode => '0600',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
 }
