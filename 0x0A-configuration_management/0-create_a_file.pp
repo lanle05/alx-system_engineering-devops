812158ca696514cbe06586186f96e1ea671d1eb0
Omobolanle Shaibu <omobolanle.shaibu@gmail.com>
2023-10-03 07:52:32 -0700

    lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet, create a file in /tmp
 file { '/tmp/school':
-  ensure  => 'file',      
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data', 
+  group   => 'www-data',
   content => 'I love Puppet',
 }
